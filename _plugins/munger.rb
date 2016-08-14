cb4694cf85f64b1da294f613e68fa3f6a5fee623
David Bindel <bindel@cs.cornell.edu>
2016-08-14 19:11:33 -0400

    Updated data munging.

---
+++ _plugins/munger.rb
@@ -0,0 +1,40 @@
+module JekyllBindel
+  class DataMunger < Jekyll::Generator
+
+    #
+    # Link global class data into class offering data (and vice-versa)
+    #
+    def munge_classes(site)
+      class_data = site.data['classes']
+      class_data['active'].each do |c|
+        class_data[c]['offered'] = []
+      end
+      class_data['current'].each do |c|
+        class_data[c['course']].update(c)
+      end
+      class_data['previous'].each do |c|
+        course = class_data[c['course']]
+        if c.key?('semester') and course.key?('offered') then
+          course['offered'] << c
+        end
+      end
+    end
+
+    def munge_talks(site)
+      require 'date'
+      talks = site.data['present']
+      talks.each do |talk|
+        talk['year'] = talk['file'][0,4]
+        talk['month'] = talk['file'][5,6].to_i
+        talk['month_abbrev'] = Date::ABBR_MONTHNAMES[talk['month']]
+        talk['uid'] = talk['file'].tr("/", "_")
+      end
+    end
+
+    def generate(site)
+      munge_classes(site)
+      munge_talks(site)
+    end
+
+  end
+end
